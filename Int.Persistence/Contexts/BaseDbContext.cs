bb2e34b45d20dd1fed5903ae831b6261f1c3f2d1
UurZer <ozerugur17@gmail.com>
2023-11-26 21:23:47 +0300

    #General - Registration

---
+++ Int.Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Int.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Int.Persistence.Contexts;
+
+public class BaseDbContext : DbContext
+{
+    protected IConfiguration Configuration { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
+    {
+        Configuration = configuration;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
